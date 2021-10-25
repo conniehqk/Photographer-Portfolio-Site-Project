f3402c61f635725fb185a5cde41d6c08daa3145a
Aristela Marku <aristela.marku@gmail.com>
2021-10-25 23:08:54 +0200

    changings

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import { Container, Image, Row } from "react-bootstrap";
 
 function Portfolio() {
+    const[imageList,setImageList]=useState([])
+    
+    useEffect(()=>{
+       fetch(" http://localhost:3000/images")
+       .then(res=>res.json())
+       .then(data=>{
+           console.log(data)
+           setImageList(data)
+       })
+    },[])
+
     return(
         <Container>
             <h1>Portfolio Page</h1>
