e70835c59d5461dc995fe74c4c9ac1986954861e
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-25 11:24:52 -0400

    deleted p elements

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,11 @@
-import { Container, Image, Row } from "react-bootstrap";
+import { Container, Image, Row } from 'react-bootstrap';
 
 function Contact() {
-    return(
-        <Container>
-            <h1>Contact Page</h1>
-        </Container>
-    )
+	return (
+		<Container>
+			<h1>Contact Page</h1>
+		</Container>
+	);
 }
 
-export default Contact
+export default Contact;
