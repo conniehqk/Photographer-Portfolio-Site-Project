ad9f1cae0ef88d6a957bd11dca25892f75a1cd25
Aristela Marku <aristela.marku@gmail.com>
2021-10-26 17:59:30 +0200

    working in profolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,68 +1,100 @@
 import { useEffect, useState } from "react";
-import PhotoCard from "./PhotoCard"
-import{Container, FloatingLabel,Form} from "react-bootstrap"
+import PhotoCard from "./PhotoCard";
+import { Container,FloatingLabel,Form} from "react-bootstrap";
 import ReactStars from "react-rating-stars-component";
 
-
 function Portfolio() {
-    const[imageList,setImageList]=useState([])
-    
-    useEffect(()=>{
-       fetch(" http://localhost:3000/images")
-       .then(res=>res.json())
-       .then(data=>{
-           console.log(data)
-           setImageList(data)
-       })
-    },[])
+  const [imageList, setImageList] = useState([]);
+  const [rating, setRating] = useState(null);
+  const [comment,setComment]=useState('')
+  const [submittedData, setSubmittedData] = useState([])
 
-    const photoToRender=imageList.map((photo)=>{
-        return <PhotoCard  photo={photo}/>
-    })
+  console.log(rating);
+  console.log(comment)
+  useEffect(() => {
+    fetch(" http://localhost:3000/images")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setImageList(data);
+      });
+  }, []);
 
-    const ratingChanged = (newRating) => {
-      console.log(newRating);
-    };
-    
+  const photoToRender = imageList.map((photo) => {
+    return <PhotoCard photo={photo} />;
+  });
 
+  const ratingChanged = (data) => {
+    setRating(data);
+  };
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const newObj={
+      rating:rating,
+      comment:comment
+    }
+    const dataarray=[...submittedData,newObj]
+    setSubmittedData(dataarray)
+    console.log("newObje", newObj)
+  }
+
+  const renderCommentsAndrating=submittedData.map((item)=>{
     return(
-        <Container>
-        <div className="checkbox">
-        <form>
-        <label>Wedding</label>
-        <input type="checkbox" name="weddings"></input>
-        <label>Portraits</label>
-        <input type="checkbox" name="portraits"></input>
-        <label>Martenity</label>
-        <input type="checkbox" name="maternity"></input>
-        <label>Family</label>
-        <input type="checkbox" name="family"></input>
-        </form>
-        </div>
-           <div className="cards">
-            {photoToRender}
-            </div>
-        <div>
-        <ReactStars
-    count={5}
-    onChange={ratingChanged}
-    size={24}
-    activeColor="#ffd700"
-      />
- 
-        <>
-  <FloatingLabel controlId="floatingTextarea2" label="Comments">
-    <Form.Control
-      as="textarea"
-      placeholder="Leave a comment here"
-      style={{ height: '100px', width:'600px'}}
-    />
-  </FloatingLabel>
-</>
-        </div>
-        
-        </Container>
+      <div>
+        <h3>Rating:{item.rating} ⭐</h3>
+        <p>MyComment: {item.comment} </p>
+      </div>
     )
+  })
+
+  return (
+    <Container>
+      <div className="checkbox">
+        <form>
+          <label>Wedding</label>
+          <input type="checkbox" name="weddings"></input>
+          <label>Portraits</label>
+          <input type="checkbox" name="portraits"></input>
+          <label>Martenity</label>
+          <input type="checkbox" name="maternity"></input>
+          <label>Family</label>
+          <input type="checkbox" name="family"></input>
+        </form>
+      </div>
+      <div className="cards">{photoToRender}</div>
+      <div>
+        <ReactStars
+          count={5}
+          value={rating}
+          onChange={(data) => ratingChanged(data)}
+          size={24}
+          activeColor="#ffd700"
+        />
+
+        <>
+        {/* <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e)=>setComment(e.target.value)} value={comment} />
+        
+        <button type="submit">Submit</button>
+        </form> */}
+        <Form onSubmit={handleSubmit}>
+          <FloatingLabel controlId="floatingTextarea2" label="Comments"  >
+            <Form.Control
+             
+              as="textarea"
+              placeholder="Leave a comment here"
+              style={{ height: "100px", width: "600px" }}
+              onChange={(e)=>setComment(e.target.value)}
+            />
+            <button>Submit</button>
+          </FloatingLabel>
+          </Form>
+        </>
+      </div>
+      {renderCommentsAndrating}
+    </Container>
+  );
 }
 
 export default Portfolio;
