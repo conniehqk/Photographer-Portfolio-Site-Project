5d32a867d6eee959825010f5a6266ba1d0be204a
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-26 10:50:35 -0400

    Staging small

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,68 +1,66 @@
-import { useEffect, useState } from "react";
-import PhotoCard from "./PhotoCard"
-import{Container, FloatingLabel,Form} from "react-bootstrap"
-import ReactStars from "react-rating-stars-component";
-
+import { useEffect, useState } from 'react';
+import PhotoCard from './PhotoCard';
+import { Container, FloatingLabel, Form } from 'react-bootstrap';
+import ReactStars from 'react-rating-stars-component';
 
 function Portfolio() {
-    const[imageList,setImageList]=useState([])
-    
-    useEffect(()=>{
-       fetch(" http://localhost:3000/images")
-       .then(res=>res.json())
-       .then(data=>{
-           console.log(data)
-           setImageList(data)
-       })
-    },[])
+	const [imageList, setImageList] = useState([]);
 
-    const photoToRender=imageList.map((photo)=>{
-        return <PhotoCard  photo={photo}/>
-    })
+	useEffect(() => {
+		fetch(' http://localhost:3000/images')
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				setImageList(data);
+			});
+	}, []);
 
-    const ratingChanged = (newRating) => {
-      console.log(newRating);
-    };
-    
+	const photoToRender = imageList.map((photo) => {
+		return <PhotoCard photo={photo} />;
+	});
 
-    return(
-        <Container>
-        <div className="checkbox">
-        <form>
-        <label>Wedding</label>
-        <input type="checkbox" name="weddings"></input>
-        <label>Portraits</label>
-        <input type="checkbox" name="portraits"></input>
-        <label>Martenity</label>
-        <input type="checkbox" name="maternity"></input>
-        <label>Family</label>
-        <input type="checkbox" name="family"></input>
-        </form>
-        </div>
-           <div className="cards">
-            {photoToRender}
-            </div>
-        <div>
-        <ReactStars
-    count={5}
-    onChange={ratingChanged}
-    size={24}
-    activeColor="#ffd700"
-      />
- 
-        <>
-  <FloatingLabel controlId="floatingTextarea2" label="Comments">
-    <Form.Control
-      as="textarea"
-      placeholder="Leave a comment here"
-      style={{ height: '100px', width:'600px'}}
-    />
-  </FloatingLabel>
-</>
-        </div>
-        
-        </Container>
-    )
+	const ratingChanged = (newRating) => {
+		console.log(newRating);
+	};
+
+	return (
+		<Container>
+			<div className="checkbox">
+				<form>
+					<label>Wedding</label>
+					<input type="checkbox" name="weddings"></input>
+					<label>Portraits</label>
+					<input type="checkbox" name="portraits"></input>
+					<label>Martenity</label>
+					<input type="checkbox" name="maternity"></input>
+					<label>Family</label>
+					<input type="checkbox" name="family"></input>
+				</form>
+			</div>
+			<div className="cards">{photoToRender}</div>
+			<div>
+				<ReactStars
+					count={5}
+					onChange={ratingChanged}
+					size={24}
+					activeColor="#ffd700"
+				/>
+
+				<>
+					<FloatingLabel
+						controlId="floatingTextarea2"
+						label="Comments"
+					>
+						<Form.Control
+							as="textarea"
+							placeholder="Leave a comment here"
+							style={{ height: '100px', width: '600px' }}
+						/>
+					</FloatingLabel>
+				</>
+			</div>
+		</Container>
+	);
 }
 
 export default Portfolio;
